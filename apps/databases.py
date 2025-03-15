5c36c6ca1421d272f5c4b745a86a9b4e6a3c5d93
nielrya4 <nielrya4@isu.edu>
2025-03-14 23:48:25 -0600

    minor updates- This is v2.0

--- apps/databases.py
+++ apps/databases.py
@@ -6,11 +6,8 @@ from flask_login import login_required
 def register(app):
     @app.route('/databases/<string:file>', methods=['GET'])
     @login_required
-    def load_databases(file):
+    def load_database(file):
         df_sheets = pd.read_excel(f"static/databases/{file}.xlsx", sheet_name=None, header=None)
-
-        # Extracting the first sheet only for Handsontable
         first_sheet_name, first_df = next(iter(df_sheets.items()))
         first_df = first_df.dropna(how="all").fillna("").reset_index(drop=True)
-
         return jsonify(first_df.values.tolist())
