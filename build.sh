5b6699246eb8350377790cfffc1f47455769b600
nielrya4 <nielrya4@isu.edu>
2025-03-25 20:10:30 -0600

    minor updates to v 2.0

--- build.sh
+++ build.sh
@@ -1,11 +1,11 @@
 echo "Updating dztoolbox code..."
 git pull
 echo "stopping dztoolbox..."
-sudo systemctl stop dztoolbox
+sudo systemctl stop uwsgi-dztoolbox
+sudo systemctl stop cloudflared
 echo "reloading daemons..."
 sudo systemctl daemon-reload
 echo "starting up dztoolbox..."
-sudo systemctl start dztoolbox
-echo "restarting nginx..."
-sudo systemctl restart nginx
+sudo systemctl start uwsgi-dztoolbox
+sudo systemctl start cloudflared
 echo "dztoolbox is now up and running"
